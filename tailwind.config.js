2077f04414a3af8f336af6075187ca3cfb556ec2
EAncoHuaracha <eancohu@unsa.edu.pe>
2024-10-16 14:16:04 -0500

    Adding color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'primary': '#04662E',
+      'secondary': '#C1D005',
+      'tertiary': '#EABFB5'
+    },
     extend: {},
   },
   plugins: [],
