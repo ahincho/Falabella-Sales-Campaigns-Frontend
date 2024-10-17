02514d42ad8753d615c609d285068e11e705ef13
erick14ah1 <erick14ah1@gmail,com>
2024-10-17 11:13:02 -0500

    Updanting colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      'primary': '#04662E',
-      'secondary': '#C1D005',
-      'tertiary': '#EABFB5',
-      'gray': '#D9D9D9',
+    extend: {
+      colors: {
+        'primary': '#04662E',
+        'secondary': '#C1D005',
+        'tertiary': '#EABFB5',
+        'gray': '#D9D9D9',
+      }
     },
-    extend: {},
   },
   plugins: [],
 }
