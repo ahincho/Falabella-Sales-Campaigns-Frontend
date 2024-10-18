0d57afc46dc490faa69f7db2ff52c7aed03d288e
EAncoHuaracha <eancohu@unsa.edu.pe>
2024-10-17 23:46:45 -0500

    Adding client and coupon models

---
+++ src/app/core/models/client.model.ts
@@ -0,0 +1,9 @@
+export interface Client {
+  couponId: string;
+  clientId: string;
+  campaign: string;
+  cuponType: string;
+  couponCodeBar: string;
+  couponCodeWeb: string;
+  date: string;
+}
