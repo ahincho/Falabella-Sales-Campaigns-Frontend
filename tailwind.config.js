d56eacd19b48bf9a747e273be867c8d7bf2b94f5
EAncoHuaracha <eancohu@unsa.edu.pe>
2024-10-22 13:07:02 -0500

    adding values for responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = {
         'tertiary': '#EABFB5',
         'gray': '#D9D9D9',
         'text-p': "#FFFFFF",
+      },
+      screens: {
+        'xs': '0px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },
